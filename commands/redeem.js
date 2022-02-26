7ce9da06a13a71f6981f9aeb5283bb7593e7f009
noah <neoptunium@gmail.com>
2022-02-26 17:11:36 -0600

    Add files via upload

--- commands/redeem.js
+++ commands/redeem.js
@@ -1,22 +1,33 @@
-const Discord = require("discord.js")
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 const config = require('../config.json')
-module.exports.run = async(client, message, args) => {
-    const RoleToAdd = message.guild.roles.cache.get(config.roleID);
-    const Filter = (reaction, user) => user.id == message.author.id;
-    const Embed = new Discord.MessageEmbed()
-         .setColor(config.embedColor)
-         .setTitle(`Redeem Role`)
-         .setDescription(`React to get the **${RoleToAdd.name}** role!`)
-         .setThumbnail('https://tenor.com/view/scammer-scam-redeeming-whyareyouredeeming-kitboga-gif-19964723')
-     const reactionMessage = await message.channel.send(Embed);
-     await reactionMessage.react("✅");
-     reactionMessage.awaitReactions(Filter, { max: 1, time: 30000, errors: ["time"] }).then(collected => {
-         const reaction = collected.first();
-         switch (reaction.emoji.name) {
-             case "✅":
-                 if (message.member.roles.cache.has(RoleToAdd.id)) { return message.channel.send(`You already have the ${RoleToAdd.name} role!`) };
-                 message.member.roles.add(RoleToAdd).then(message.channel.send(`You now have the ${RoleToAdd.name} role!`));
-                 break
-         }
-     })
-}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('redeem')
+		.setDescription(`Redeem your cool role!`),
+	async execute(interaction) {
+    const RoleToAdd = interaction.guild.roles.cache.get(config.roleID);
+	
+	const exampleEmbed = new MessageEmbed()
+	.setColor(config.embedColor)
+	.setTitle(`Redeem Role`)
+	.setDescription(`Congratulations, you now have the **${RoleToAdd.name}** role!`)
+	.setThumbnail('https://cdn.discordapp.com/attachments/942989506209005600/947267234001219726/scammer-scam.gif')
+	.setTimestamp();
+
+    const exampleEmbed2 = new MessageEmbed()
+	.setColor(config.embedColor)
+	.setTitle(`Redeem Role`)
+	.setDescription(`You already have the **${RoleToAdd.name}** role!`)
+	.setThumbnail('https://cdn.discordapp.com/attachments/942989506209005600/947267234001219726/scammer-scam.gif')
+	.setTimestamp();
+
+    if (interaction.member.roles.cache.has(RoleToAdd.id)) { return interaction.reply({ embeds: [exampleEmbed2]});
+    }
+    
+	interaction.member.roles.add(config.roleID);
+	  
+	interaction.reply({ embeds: [exampleEmbed]});
+	},
+};
--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
-const client = new Discord.Client();
+const { Client, Collection, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { token } = require('./config.json');
+const fs = require('fs');
 
-const config = require("./config.json");
-client.config = config;
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-        const event = require(`./events/${file}`);
-        let eventName = file.split(".")[0];
-        console.log(`Attempting to load event ${eventName}`);
-        client.on(eventName, event.bind(null, client));
-    });
-});
-client.commands = new Enmap();
-fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-        if (!file.endsWith(".js")) return;
-        let props = require(`./commands/${file}`);
-        let commandName = file.split(".")[0];
-        console.log(`Attempting to load command ${commandName}`);
-        client.commands.set(commandName, props);
-    });
-});
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+client.commands = new Collection();
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);}
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) { client.once(event.name, (...args) => event.execute(...args));
+    } else {client.on(event.name, (...args) => event.execute(...args));}
+}
 
-client.login(config.token);
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return; try { await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+client.login(token);
